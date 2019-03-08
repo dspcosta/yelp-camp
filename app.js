78165a42a837e052db30b5f31d57b3efc3f53ef5
Daniel Costa <dcostabus@gmail.com>
2019-03-08 14:30:41 -0300

    Updated app.post so it can add new image and name

--- app.js
+++ app.js
@@ -8,22 +8,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist/css"));
 
+let campgrounds = [
+    { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg" },
+    { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg" }
+];
+
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    let campgrounds = [
-        { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg" },
-        { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg" },
-        { name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg" }
-    ];
-
     res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.post("/campgrounds", function(req, res) {
-    res.send("YOU HIT POST ROUTE!");
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = { name: name, image: image };
+
+    campgrounds.push(newCampground);
+
+    res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res) {
