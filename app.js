03e0a2598b9a50560fbd853b5aed41cc96a61158
Daniel Costa <dcostabus@gmail.com>
2019-03-13 14:36:42 -0300

    Added mongoose to use MongoDB. Updated all necessary functions to work with the db. Included description field

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist/css"));
 
-let campgrounds = [
-    { name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg" },
-    { name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_960_720.jpg" },
-    { name: "Mountain Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg" }
-];
+// SCHEMA SETUP
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX ROUTE
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    Campground.find({}, function(err, allcampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { campgrounds: allcampgrounds });
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     let name = req.body.name;
     let image = req.body.image;
-    let newCampground = { name: name, image: image };
+    let description = req.body.description;
+    let newCampground = { name: name, image: image, description: description };
 
-    campgrounds.push(newCampground);
-
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
+// NEW - form to create new Campground
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampgroud) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", { campground: foundCampgroud });
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
